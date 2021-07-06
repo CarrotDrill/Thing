f20c070674c1f0ce19e7e30d9aca33b6dbe3186a
CarrotDrill <86727101+CarrotDrill@users.noreply.github.com>
2021-07-06 14:48:35 +0000

    Made program that counts to 10

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pygame
 
-first name = input("What is your given name?")
-last name = input("What is your surname?")
-print("Hello, " + first name + last name + ".")
+my_list = []
+
+for e in range(10): 
+  print ("The count is: " + str(e+1))
+  my_list.append(e+1)
+
+# Write a program that uses a loop fill an empty list with 10
+#numbers and prints the list to the console.
