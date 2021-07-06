9e57241c86ba3bcf698fee0c1a684fc92ea8dc74
CarrotDrill <86727101+CarrotDrill@users.noreply.github.com>
2021-07-06 14:50:31 +0000

    Imported random, added random number generator (0-10)

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import pygame
+import random
 
-my_list = []
-
-for e in range(10): 
-  print ("The count is: " + str(e+1))
-  my_list.append(e+1)
-
-# Write a program that uses a loop fill an empty list with 10
-#numbers and prints the list to the console.
+numero = random.randint(0,10)
+print (numero)
