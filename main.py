54050789891bfc6d96ed6bcc305ea1207822485c
CarrotDrill <86727101+CarrotDrill@users.noreply.github.com>
2021-07-06 16:11:49 +0000

    Yes

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import random
 
 numero = random.randint(0,10)
-print(numero)
 Guesses = 3 
 Win = False
 Active = True
 
-if Guesses == 0:
-  print("You're out of guesses. Restart the program to play again.")
 
-elif Active == False and Win == True:
+
+if Active == False and Win == True:
   print("That's right! You win. Restart the program to play again.")
 
 while Active == True:
@@ -17,12 +15,36 @@ while Active == True:
     num = input("Guess a random number from 1 to 10: ")
     num = int(num)
     if num == numero:
-      Active = False 
       Win = True
       Guesses = 0 
     elif num > numero:
       Guesses = Guesses - 1
-      print("You have " + str(Guesses) + " tries left. Your number is too big, so guess lower.")
+      if Guesses != 0:
+        print("You have " + str(Guesses) + " tries left. Your number is too big, so guess lower.") 
+
     elif num < numero: 
       Guesses = Guesses - 1
-      print("You have " + str(Guesses) + " tries left. Your number is too small, so guess higher.")
+      if Guesses != 0:
+        print("You have " + str(Guesses) + " tries left. Your number is too small, so guess higher.")
+
+
+  if Guesses == 0 and Win == False:
+    print("You're out of guesses. The number was " + str(numero) + ".")
+  
+  if Win == True:
+    print("That's right! You win.")
+
+  
+  
+  Repeat = input("Do you want to play again? Y/N: ")
+
+  if Repeat == "Y":
+    Guesses = 3
+    Win = False
+    print("Okay, resetting the game.")
+    numero = random.randint(0,10)
+  else: 
+    print("Ending the game.")
+    Active = False
+
+#I think it works now, thanks.
